6eb79590292a2723f27137c2aac789f50b0808e6
Pyrtle93 <xxpyrtlexx93@gmail.com>
2024-09-19 19:22:34 +0530

    vendor: Unset FULL_SYSTEM_OPTIMIZE_JAVA * Breaks screen resolution change * switch to googles face unlock Test: build, boot, no more resolution breakage after screen resolution change Change-Id: I90eaf92e144c6d3f095657682b100a56d0468153

--- config/packages.mk
+++ config/packages.mk
@@ -6,16 +6,6 @@ PRODUCT_PACKAGES += \
     OmniJaws \
     OmniStyle
 
-# Face Unlock
-PRODUCT_PACKAGES += \
-    FaceUnlock
-
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.face.sense_service=true
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
-
 # Lineage Health
 PRODUCT_COPY_FILES += \
     vendor/infinity/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
